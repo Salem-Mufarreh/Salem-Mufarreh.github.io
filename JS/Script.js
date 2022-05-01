f646f9fc31cca812df96c5c8751c85147bef6a10
Salem-Mufarreh <salemmuf9@gmail.com>
2022-05-01 21:21:03 +0300

    Fixing the navigation

--- JS/Script.js
+++ JS/Script.js
@@ -1,4 +1,4 @@
-$('document').ready(function () {
+/*$('document').ready(function () {
     var typed = new Typed('#typed', {
         //stringsElement: '#typed-strings',
         backSpeed: 80,
@@ -7,14 +7,30 @@ $('document').ready(function () {
         strings: ['Web Developer ', 'Full Stack Developer ', 'Software Developer'],
         loop: true
     });
-});
+}); */
 
 function download() {
     window.location.href = "./Files/Resume.pdf";
 
 }
 
-const header = document.querySelector("header");
+const navigation = document.querySelector(".primary_navigation");
+const navtoggle = document.querySelector(".mobile-toggle");
+
+
+navtoggle.addEventListener("click", () => {
+    const visibility = navigation.getAttribute("data-visible");
+    if (visibility === "false") {
+        navigation.setAttribute("data-visible", true);
+        navtoggle.setAttribute("aria-expanded", true);
+    }
+    else {
+        navigation.setAttribute('data-visible', false);
+        navtoggle.setAttribute("aria-expanded", false);
+    }
+});
+
+/*const header = document.querySelector("header");
 const sectionOne = document.querySelector(".home");
 
 const sectionOneOptions = {
@@ -26,9 +42,11 @@ const sectionOneObserver = new IntersectionObserver(function (
     sectionOneObserver
 ) {
     entries.forEach(entry => {
-        console.log(entry.target);
+        if (entry.isIntersecting) {
+
+        }
     });
 },
     sectionOneOptions);
 
-sectionOneObserver.observe(sectionOne);
+sectionOneObserver.observe(sectionOne); */
